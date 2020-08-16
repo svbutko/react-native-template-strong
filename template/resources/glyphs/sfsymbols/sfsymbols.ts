7e8d0c53afff9af5bfcdde7f101c5e6b6b3d8313
svbutko <svbutko@hotmail.com>
2020-08-16 22:42:46 +0300

    Add SF Symbols Health

--- template/resources/glyphs/sfsymbols/sfsymbols.ts
+++ template/resources/glyphs/sfsymbols/sfsymbols.ts
@@ -1,8 +1,10 @@
 import {Indices} from "./categories/indices";
 import {Maths} from "./categories/maths";
 import {Shapes} from "./categories/shapes";
+import {Health} from "./categories/health";
 
 export const SFSymbols = {
+    health: Health,
     shapes: Shapes,
     indices: Indices,
     maths: Maths,
