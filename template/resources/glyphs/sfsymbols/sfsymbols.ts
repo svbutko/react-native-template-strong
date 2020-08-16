ded0c716ac900a614addf1acccc8e1ce92a9e20c
svbutko <svbutko@hotmail.com>
2020-08-16 22:51:50 +0300

    Add SF Symbols Commerce

--- template/resources/glyphs/sfsymbols/sfsymbols.ts
+++ template/resources/glyphs/sfsymbols/sfsymbols.ts
@@ -3,8 +3,10 @@ import {Maths} from "./categories/maths";
 import {Shapes} from "./categories/shapes";
 import {Health} from "./categories/health";
 import {Time} from "./categories/time";
+import {Commerce} from "./categories/commerce";
 
 export const SFSymbols = {
+    commerce: Commerce,
     time: Time,
     health: Health,
     shapes: Shapes,
