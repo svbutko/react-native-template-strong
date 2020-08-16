1ce26d8f2c883b047b1c5265b6a1c7ff6d0670ae
svbutko <svbutko@hotmail.com>
2020-08-16 22:37:00 +0300

    Add SF Symbols Shapes

--- template/resources/glyphs/sfsymbols/sfsymbols.ts
+++ template/resources/glyphs/sfsymbols/sfsymbols.ts
@@ -1,7 +1,9 @@
 import {Indices} from "./categories/indices";
 import {Maths} from "./categories/maths";
+import {Shapes} from "./categories/shapes";
 
 export const SFSymbols = {
+    shapes: Shapes,
     indices: Indices,
     maths: Maths,
 };
