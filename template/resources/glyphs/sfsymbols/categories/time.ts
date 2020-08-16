d87c70242f38a48e301bd49ec21653bb20b27336
svbutko <svbutko@hotmail.com>
2020-08-16 22:46:02 +0300

    Add SF Symbols Time

---
+++ template/resources/glyphs/sfsymbols/categories/time.ts
@@ -0,0 +1,12 @@
+export enum Time {
+    alarm = "􀐭",
+    alarmFill = "􀐮",
+    clock = "􀐫",
+    clockArrowCirclePath = "􀣔",
+    clockFill = "􀐬",
+    deskClock = "􀡑",
+    deskClockFill = "􀡒",
+    stopwatch = "􀐯",
+    stopwatchFill = "􀐰",
+    timer = "􀐱",
+}
--- template/resources/glyphs/sfsymbols/sfsymbols.ts
+++ template/resources/glyphs/sfsymbols/sfsymbols.ts
@@ -2,8 +2,10 @@ import {Indices} from "./categories/indices";
 import {Maths} from "./categories/maths";
 import {Shapes} from "./categories/shapes";
 import {Health} from "./categories/health";
+import {Time} from "./categories/time";
 
 export const SFSymbols = {
+    time: Time,
     health: Health,
     shapes: Shapes,
     indices: Indices,
