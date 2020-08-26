99b21a46af4f063da3e301274dd6f3ca33522f4d
svbutko <svbutko@hotmail.com>
2020-08-26 20:43:42 +0300

    Add the babel plugin to remove console logs from the production mode

--- template/babel.config.js
+++ template/babel.config.js
@@ -1,4 +1,11 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['react-native-reanimated/plugin']
+  presets: ["module:metro-react-native-babel-preset"],
+  plugins: ["react-native-reanimated/plugin"],
+  env: {
+    production: {
+      plugins: [
+          "transform-remove-consoles"
+      ],
+    }
+  }
 };
