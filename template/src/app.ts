34321e62d2a2d1e0bac90490cc542cd2f67abe5d
svbutko <svbutko@hotmail.com>
2021-07-20 15:01:09 +0300

    Fix app navigation initialization

--- template/src/app.ts
+++ template/src/app.ts
@@ -1,80 +1,16 @@
-import {Pages, registerComponents, setDefaultOptions, Tabs} from "./navigation";
+import {registerComponents, setDefaultOptions} from "./navigation";
 import {Navigation} from "react-native-navigation";
-import {localization} from "./common/localization";
+import {setLanguage} from "./common/localization";
 import {setDefaultOrientation} from "./common/helpers";
+import {setInitialRoot} from "./navigation/roots";
 
 export function initializeApp() {
-    setDefaultOrientation();
-    registerComponents();
-    setDefaultOptions();
-    Navigation.events().registerAppLaunchedListener(async () => {
-        Navigation.dismissAllModals();
-        setRoot();
-    });
-}
-
-function setRoot() {
-    Navigation.setRoot({
-        root: {
-            bottomTabs: {
-                id: Pages.tabs.id,
-                children: [
-                    {
-                        stack: {
-                            id: Tabs.main.id,
-                            children: [
-                                {
-                                    component: {
-                                        id: Pages.main.id,
-                                        name: Pages.main.name,
-                                    }
-                                }
-                            ],
-                            options: {
-                                bottomTab: {
-                                    text: localization.pages.main
-                                },
-                            },
-                        },
-                    },
-                    {
-                        stack: {
-                            id: Tabs.demo.id,
-                            children: [
-                                {
-                                    component: {
-                                        id: Pages.demo.id,
-                                        name: Pages.demo.name,
-                                    }
-                                }
-                            ],
-                            options: {
-                                bottomTab: {
-                                  text: localization.pages.demo
-                                },
-                            },
-                        },
-                    },
-                    {
-                        stack: {
-                            id: Tabs.more.id,
-                            children: [
-                                {
-                                    component: {
-                                        id: Pages.more.id,
-                                        name: Pages.more.name,
-                                    }
-                                }
-                            ],
-                            options: {
-                                bottomTab: {
-                                    text: localization.pages.more
-                                },
-                            },
-                        },
-                    }
-                ],
-            },
-        }
-    });
+  setLanguage();
+  setDefaultOrientation();
+  registerComponents();
+  setDefaultOptions();
+  Navigation.events().registerAppLaunchedListener(async () => {
+    Navigation.dismissAllModals();
+    setInitialRoot();
+  });
 }
