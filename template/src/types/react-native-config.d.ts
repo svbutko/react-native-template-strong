cd4cdeeb18706d1339d8fe0017ac7b2ce1ab0f0d
svbutko <svbutko@hotmail.com>
2021-07-21 16:26:49 +0300

    Add react-native-version, add the environment variable

--- template/src/types/react-native-config.d.ts
+++ template/src/types/react-native-config.d.ts
@@ -1,6 +1,14 @@
 declare module "react-native-config" {
+  export enum AppEnvironment {
+    development = "development",
+    test = "test",
+    staging = "staging",
+    production = "production",
+  }
+
   interface EnvironmentVariables {
     API_URL: string;
+    ENVIRONMENT: AppEnvironment;
   }
 
   export const Config: EnvironmentVariables;
