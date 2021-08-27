a10213e094d3f02fe1abab66690f3b8198882c18
svbutko <svbutko@hotmail.com>
2021-08-27 16:44:32 +0300

    Change LoadingComponent style

--- template/src/common/components/LoadingComponent.tsx
+++ template/src/common/components/LoadingComponent.tsx
@@ -1,22 +1,20 @@
-import {ActivityIndicator, Text, TextStyle, View, StyleSheet} from "react-native";
+import {ActivityIndicator, Text, View} from "react-native";
 import React from "react";
 import {localization} from "../localization";
-import {Colors, CommonStyles} from "../../core/theme";
+import {CommonStyles, PlatformColorsAndroid, PlatformColorsIOS} from "../../core/theme";
+import {platformNativeColor} from "../helpers";
 
 export const LoadingComponent = () => {
   return (
     <View style={CommonStyles.flexCenter}>
-      <ActivityIndicator animating={true} color={Colors.black} size={"small"} />
-      <Text style={styles.text} numberOfLines={1}>
+      <ActivityIndicator
+        animating={true}
+        color={platformNativeColor(PlatformColorsIOS.label, PlatformColorsAndroid.primary)}
+        size={"small"}
+      />
+      <Text style={CommonStyles.normalText} numberOfLines={1}>
         {localization.common.loading}
       </Text>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  text: {
-    ...CommonStyles.normalText,
-    color: Colors.black,
-  } as TextStyle,
-});
