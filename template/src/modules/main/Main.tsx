0e668da5a07995bb5401721f4f0fa8d29207bbe8
svbutko <svbutko@hotmail.com>
2021-08-28 13:35:41 +0300

    Remove Demo, Mand and More content

--- template/src/modules/main/Main.tsx
+++ template/src/modules/main/Main.tsx
@@ -1,51 +1,11 @@
 import {NavigationFunctionComponent} from "react-native-navigation";
-import {Platform, PlatformColor, ScrollView, Text, TextStyle, View, ViewStyle, StyleSheet} from "react-native";
-import {DebugInstructions, ReloadInstructions} from "react-native/Libraries/NewAppScreen";
+import {ScrollView} from "react-native";
 import React from "react";
-import {PlatformColorsAndroid, PlatformColorsIOS} from "../../core/theme";
+import {CommonStyles} from "../../core/theme";
 import {localization} from "../../common/localization";
 
 export const Main: NavigationFunctionComponent = (): JSX.Element => {
-  return (
-    <ScrollView contentInsetAdjustmentBehavior="automatic" style={styles.scrollView}>
-      <Text style={styles.sectionTitle}>Step One</Text>
-      <Text style={styles.sectionDescription}>
-        Edit <Text style={styles.highlight}>App.js</Text> to change this screen and then come back to see your edits.
-      </Text>
-      <View style={styles.sectionContainer}>
-        <Text style={styles.sectionTitle}>See Your Changes</Text>
-        <Text style={styles.sectionDescription}>
-          <ReloadInstructions />
-        </Text>
-      </View>
-      <View style={styles.sectionContainer}>
-        <Text style={styles.sectionTitle}>Debug</Text>
-        <Text style={styles.sectionDescription}>
-          <DebugInstructions />
-        </Text>
-      </View>
-      <View style={styles.sectionContainer}>
-        <Text style={styles.sectionTitle}>Learn More</Text>
-        <Text style={styles.sectionDescription}>Read the docs to discover what to do next:</Text>
-      </View>
-      <View style={styles.sectionContainer}>
-        <Text style={styles.sectionTitle}>Learn More</Text>
-        <Text style={styles.sectionDescription}>Read the docs to discover what to do next:</Text>
-      </View>
-      <View style={styles.sectionContainer}>
-        <Text style={styles.sectionTitle}>Learn More</Text>
-        <Text style={styles.sectionDescription}>Read the docs to discover what to do next:</Text>
-      </View>
-      <View style={styles.sectionContainer}>
-        <Text style={styles.sectionTitle}>Learn More</Text>
-        <Text style={styles.sectionDescription}>Read the docs to discover what to do next:</Text>
-      </View>
-      <View style={styles.sectionContainer}>
-        <Text style={styles.sectionTitle}>Learn More</Text>
-        <Text style={styles.sectionDescription}>Read the docs to discover what to do next:</Text>
-      </View>
-    </ScrollView>
-  );
+  return <ScrollView contentInsetAdjustmentBehavior={"automatic"} style={CommonStyles.flex1} />;
 };
 
 Main.options = {
@@ -61,49 +21,3 @@ Main.options = {
     },
   },
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    flex: 1,
-    paddingHorizontal: 18,
-    ...Platform.select({
-      ios: {
-        backgroundColor: PlatformColor(PlatformColorsIOS.systemBackground),
-      },
-      android: {
-        backgroundColor: PlatformColor(PlatformColorsAndroid.background),
-      },
-    }),
-  } as ViewStyle,
-  sectionContainer: {
-    marginTop: 32,
-  } as ViewStyle,
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: "600",
-    ...Platform.select({
-      ios: {
-        color: PlatformColor(PlatformColorsIOS.label),
-      },
-      android: {
-        color: PlatformColor(PlatformColorsAndroid.primary),
-      },
-    }),
-  } as TextStyle,
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: "400",
-    ...Platform.select({
-      ios: {
-        color: PlatformColor(PlatformColorsIOS.label),
-      },
-      android: {
-        color: PlatformColor(PlatformColorsAndroid.secondary),
-      },
-    }),
-  } as TextStyle,
-  highlight: {
-    fontWeight: "700",
-  } as TextStyle,
-});
