8ad8d754192e7542b3d0bbb81936305c93d69331
svbutko <svbutko@hotmail.com>
2021-08-30 15:15:18 +0300

    Remove the intent config configuration changes

--- template/android/app/src/main/java/com/helloworld/MainActivity.java
+++ template/android/app/src/main/java/com/helloworld/MainActivity.java
@@ -1,16 +1,11 @@
 package com.helloworld;
 
 import com.reactnativenavigation.NavigationActivity;
-import android.content.Intent;
 import android.content.res.Configuration;
 
 public class MainActivity extends NavigationActivity {
-
   @Override
   public void onConfigurationChanged(Configuration newConfig) {
       super.onConfigurationChanged(newConfig);
-      Intent intent = new Intent("onConfigurationChanged");
-      intent.putExtra("newConfig", newConfig);
-      this.sendBroadcast(intent);
   }
 }
