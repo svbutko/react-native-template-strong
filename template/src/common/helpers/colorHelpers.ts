08cc3705ca7840eb19d336b95423d391247a24b1
svbutko <svbutko@hotmail.com>
2021-08-30 13:54:03 +0300

    Make the color helpers to accept undefined values

--- template/src/common/helpers/colorHelpers.ts
+++ template/src/common/helpers/colorHelpers.ts
@@ -1,16 +1,22 @@
 import {PlatformColor} from "react-native";
 import {Colors, isIos, PlatformColorsAndroid, PlatformColorsIOS} from "../../core/theme";
 
-export function platformNativeColor(iosColor: PlatformColorsIOS, androidColor: PlatformColorsAndroid) {
-  return PlatformColor(isIos ? iosColor : androidColor);
+export function platformNativeColor(iosColor?: PlatformColorsIOS, androidColor?: PlatformColorsAndroid) {
+  const selectedColor = isIos ? iosColor : androidColor;
+
+  return selectedColor != null ? PlatformColor(selectedColor) : undefined;
 }
 
-export function platformLocalColor(iosColor: Colors, androidColor: Colors) {
+export function platformLocalColor(iosColor?: Colors, androidColor?: Colors) {
   return isIos ? iosColor : androidColor;
 }
 
-export function platformMixedColor(iosColor: Colors | PlatformColorsIOS, androidColor: Colors | PlatformColorsAndroid) {
+export function platformMixedColor(iosColor?: Colors | PlatformColorsIOS, androidColor?: Colors | PlatformColorsAndroid) {
   const selectedColor = isIos ? iosColor : androidColor;
 
-  return Object.values(Colors).includes(selectedColor as Colors) ? (selectedColor as Colors) : PlatformColor(selectedColor);
+  if (selectedColor != null) {
+    return Object.values(Colors).includes(selectedColor as Colors) ? (selectedColor as Colors) : PlatformColor(selectedColor);
+  } else {
+    return undefined;
+  }
 }
