e090857af5643f5bac5f466b6fd9f5d265e1e985
svbutko <svbutko@hotmail.com>
2021-08-31 17:17:42 +0300

    Add stories.ts into eslint ignore

--- template/storybook/stories.ts
+++ template/storybook/stories.ts
@@ -1,10 +1,6 @@
 if (__DEV__) {
-  // eslint-disable-next-line import/no-unassigned-import
   require("./stories/Theme.stories");
-  // eslint-disable-next-line import/no-unassigned-import
   require("./stories/Formatter.stories");
-  // eslint-disable-next-line import/no-unassigned-import
   require("./stories/Resources.stories");
-  // eslint-disable-next-line import/no-unassigned-import
   require("./stories/Dialogs.stories");
 }
