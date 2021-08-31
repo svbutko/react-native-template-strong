b7b3fb8adc1c66bb68e78549ea2e0709690afd51
svbutko <svbutko@hotmail.com>
2021-08-31 15:27:54 +0300

    Add index.js to eslint ignore

--- template/index.js
+++ template/index.js
@@ -1,22 +1,15 @@
-// eslint-disable-next-line import/no-unassigned-import
 import "react-native-gesture-handler";
 import {initializeApp} from "./src/app";
 
 if (__DEV__) {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
   const {configure} = require("@storybook/react-native");
-  // eslint-disable-next-line import/no-unassigned-import
   require("@storybook/addon-ondevice-knobs/register");
-  // eslint-disable-next-line import/no-unassigned-import
   require("@storybook/addon-ondevice-actions/register");
   configure(() => {
-    // eslint-disable-next-line import/no-unassigned-import
     require("./storybook/stories.ts");
   }, module);
 
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
   const DevMenu = require("react-native-dev-menu");
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
   const {showStorybook} = require("./src/navigation/helpers/showStorybook");
   DevMenu.addItem("Storybook", showStorybook);
 }
