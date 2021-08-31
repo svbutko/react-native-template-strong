a125e21f486bcdab6cd44962fbddc88119416002
svbutko <svbutko@hotmail.com>
2021-08-31 15:42:05 +0300

    Minor helpers renaming

--- template/src/common/helpers/index.ts
+++ template/src/common/helpers/index.ts
@@ -1,10 +1,10 @@
-import {getCurrentOrientation, setDefaultOrientation} from "./orientationHelper";
+import {getCurrentOrientation, setDefaultOrientation} from "./orientationHelpers";
 import {fromArray, fromMap, withNewElement, withNewElements} from "./fromMap";
 import {defaultKeyIdExtractor} from "./defaultKeyIdExtractor";
 import {calculatePage} from "./calculatePage";
 import {capitalizeFirstLetter, removeEmojis, removeHtmlTags} from "./stringsHelpers";
 import {isEmail} from "./regexHelpers";
-import {showActionSheet, showCommonDialog} from "./dialogsHelper";
+import {showActionSheet, showCommonDialog} from "./dialogsHelpers";
 import {convertCropperImageToBase64} from "./imageHelpers";
 import {InputMask} from "./inputMask";
 import {platformLocalColor, platformMixedColor, platformNativeColor} from "./colorHelpers";
