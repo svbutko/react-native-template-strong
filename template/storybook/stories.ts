9ec120216549c796dba7b8be4bf4a13bbc9839fe
svbutko <svbutko@hotmail.com>
2021-09-04 18:55:56 +0300

    Return components stories to Storybook

--- template/storybook/stories.ts
+++ template/storybook/stories.ts
@@ -2,5 +2,6 @@ if (__DEV__) {
   require("./stories/Theme.stories");
   require("./stories/Formatter.stories");
   require("./stories/Resources.stories");
+  require("./stories/Components.stories");
   require("./stories/ModalsOverlays.stories");
 }
