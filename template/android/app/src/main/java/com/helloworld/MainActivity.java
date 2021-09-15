74b643e136f418ad7f81cc117a58594536ec618c
svbutko <svbutko@hotmail.com>
2021-09-15 15:17:27 +0300

    Remove redundant on configuration change on Android

--- template/android/app/src/main/java/com/helloworld/MainActivity.java
+++ template/android/app/src/main/java/com/helloworld/MainActivity.java
@@ -1,11 +1,6 @@
 package com.helloworld;
 
 import com.reactnativenavigation.NavigationActivity;
-import android.content.res.Configuration;
 
 public class MainActivity extends NavigationActivity {
-  @Override
-  public void onConfigurationChanged(Configuration newConfig) {
-      super.onConfigurationChanged(newConfig);
-  }
 }
