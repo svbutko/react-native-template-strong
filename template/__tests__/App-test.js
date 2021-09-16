68f25cd56e6fb9c102deccd6468526c236888bed
svbutko <svbutko@hotmail.com>
2021-09-16 16:24:23 +0300

    Modify tsconfig and add react-addons-test-utils and fix jest config

--- template/__tests__/App-test.js
+++ template/__tests__/App-test.js
@@ -2,13 +2,14 @@
  * @format
  */
 
-import 'react-native';
-import React from 'react';
-import App from '../App';
+// eslint-disable-next-line import/no-unassigned-import
+import "react-native";
+import React from "react";
+import App from "../App";
 
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import renderer from "react-test-renderer";
 
-it('renders correctly', () => {
+it("renders correctly", () => {
   renderer.create(<App />);
 });
---
+++ template/jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  preset: "react-native",
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.spec.json",
+    },
+  },
+  transform: {
+    "^.+\\.jsx$": "babel-jest",
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  testRegex: "(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  testPathIgnorePatterns: ["\\.snap$", "<rootDir>/node_modules/"],
+  cacheDirectory: ".jest/cache",
+};
