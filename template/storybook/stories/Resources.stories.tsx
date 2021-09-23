cdd655e96af479a3aae662c20977ff516271d0b8
svbutko <svbutko@hotmail.com>
2021-09-23 19:53:46 +0300

    Remove SF Symbols from Storybook in favor of website

--- template/storybook/stories/Resources.stories.tsx
+++ template/storybook/stories/Resources.stories.tsx
@@ -2,15 +2,9 @@ import React from "react";
 import {storiesOf} from "@storybook/react-native";
 import {ImagesList} from "../components/ImagesList";
 import {IllustrationsResources, ImageResources} from "../../src/common/ImageResources.g";
-import {isIos} from "../../src/core/theme/commonConsts";
-import {SFSymbolsList} from "../components/SFSymbolsList";
 
 const ResourcesStories = storiesOf("Resources", module);
 
 ResourcesStories.add("ImageResources", () => <ImagesList data={Object.entries(ImageResources)} />);
 
 ResourcesStories.add("IllustrationsResources", () => <ImagesList data={Object.entries(IllustrationsResources)} />);
-
-if (isIos) {
-  ResourcesStories.add("SFSymbols", () => <SFSymbolsList />);
-}
