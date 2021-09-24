b4bdd6965a0f035fe3161d30adca3e68b28fc480
svbutko <svbutko@hotmail.com>
2021-09-24 10:01:34 +0300

    Allow undefined values for icon platform

--- template/src/common/components/IconPlatform.tsx
+++ template/src/common/components/IconPlatform.tsx
@@ -1,5 +1,5 @@
 import React, {FC, memo} from "react";
-import {isIos} from "../../core/theme/commonConsts";
+import {isAndroid, isIos} from "../../core/theme/commonConsts";
 import Icon from "react-native-vector-icons/MaterialIcons";
 import {SFSymbol, SFSymbolProps} from "react-native-sfsymbols";
 import {IconProps} from "react-native-vector-icons/Icon";
@@ -7,16 +7,18 @@ import {ColorValue, TextStyle, ViewStyle} from "react-native";
 import {SFSymbols} from "../../../resources/symbols/SFSymbols";
 
 interface IProps extends Omit<IconProps, "name" | "color">, Omit<SFSymbolProps, "name"> {
-  iosName: SFSymbols;
-  androidName: string;
+  iosName?: SFSymbols;
+  androidName?: string;
   color?: ColorValue;
   style?: TextStyle | ViewStyle;
 }
 
 export const IconPlatform: FC<IProps> = memo(({iosName, androidName, ...props}) => {
-  if (isIos) {
+  if (isIos && iosName) {
     return <SFSymbol {...props} name={iosName} />;
-  } else {
+  } else if (isAndroid && androidName) {
     return <Icon {...props} name={androidName} />;
+  } else {
+    return null;
   }
 });
