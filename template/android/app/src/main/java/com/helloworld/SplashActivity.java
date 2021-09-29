d102ea485738f21da8a81230c79e5771206169a0
svbutko <svbutko@hotmail.com>
2021-09-29 08:56:39 +0300

    Add splash to Android

---
+++ template/android/app/src/main/java/com/helloworld/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.helloworld;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+        finish();
+    }
+}
