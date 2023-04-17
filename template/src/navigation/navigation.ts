a95e1f32f811b34f1e1086bdaa4da6b26814c755
svbutko <svbutko@hotmail.com>
2023-04-17 10:41:48 +0300

    Fix ts issue with Storybook UI

--- template/src/navigation/navigation.ts
+++ template/src/navigation/navigation.ts
@@ -16,97 +16,95 @@ import {Onboarding} from "../modules/onboarding/Onboarding";
 import {Search} from "../modules/search/Search";
 import {Settings} from "../modules/settings/Settings";
 
-const StorybookUIRoot = getStorybookUI({
-  asyncStorage: null,
-});
+const StorybookUIRoot = getStorybookUI({});
 
 export function setDefaultOptions() {
-  Navigation.setDefaultOptions({
-    animations: {
-      setRoot: {
-        waitForRender: true,
-      },
-      setStackRoot: {
-        waitForRender: true,
-      },
-    },
-    layout: {
-      componentBackgroundColor: platformNativeColor(PlatformColorsIOS.secondarySystemBackground, PlatformColorsAndroid.background),
-    },
-    topBar: {
-      animate: true,
-      drawBehind: !isAndroid,
-      background: {
-        translucent: true,
-        color: platformNativeColor(PlatformColorsIOS.secondarySystemBackground, PlatformColorsAndroid.navigation),
-      },
-      title: {
-        color: platformNativeColor(undefined, PlatformColorsAndroid.onPrimaryText),
-      },
-      largeTitle: {
-        visible: false,
-      },
-      scrollEdgeAppearance: {
-        active: true,
-        noBorder: true,
-        background: {
-          translucent: true,
-          color: platformNativeColor(PlatformColorsIOS.secondarySystemBackground, PlatformColorsAndroid.navigation),
+    Navigation.setDefaultOptions({
+        animations: {
+            setRoot: {
+                waitForRender: true,
+            },
+            setStackRoot: {
+                waitForRender: true,
+            },
         },
-      },
-      searchBar: {
-        visible: false,
-        hideOnScroll: true,
-        hideTopBarOnFocus: true,
-        obscuresBackgroundDuringPresentation: true,
-      },
-      hideNavBarOnFocusSearchBar: true,
-      searchBarHiddenWhenScrolling: true,
-      searchBarPlaceholder: localization.common.search,
-      noBorder: false,
-    },
-    bottomTabs: {
-      animate: true,
-      hideShadow: false,
-      translucent: true,
-      animateTabSelection: true,
-      preferLargeIcons: false,
-      tabsAttachMode: "together",
-      backgroundColor: platformNativeColor(PlatformColorsIOS.secondarySystemBackground, PlatformColorsAndroid.navigation),
-      ...Platform.select({
-        android: {
-          translucent: false,
-          borderWidth: 1,
-          borderColor: platformNativeColor(undefined, PlatformColorsAndroid.divider),
+        layout: {
+            componentBackgroundColor: platformNativeColor(PlatformColorsIOS.secondarySystemBackground, PlatformColorsAndroid.background),
         },
-      }),
-    },
-    bottomTab: {
-      selectedTextColor: platformNativeColor(PlatformColorsIOS.systemBlue, PlatformColorsAndroid.onPrimaryText),
-      selectedIconColor: platformNativeColor(PlatformColorsIOS.systemBlue, PlatformColorsAndroid.onPrimaryText),
-      textColor: platformNativeColor(PlatformColorsIOS.secondaryLabel, PlatformColorsAndroid.onPrimaryTextOpacity),
-      iconColor: platformNativeColor(PlatformColorsIOS.secondaryLabel, PlatformColorsAndroid.onPrimaryTextOpacity),
-    },
-    statusBar: {
-      backgroundColor: platformNativeColor(undefined, PlatformColorsAndroid.statusbar),
-      visible: true,
-    },
-  });
+        topBar: {
+            animate: true,
+            drawBehind: !isAndroid,
+            background: {
+                translucent: true,
+                color: platformNativeColor(PlatformColorsIOS.secondarySystemBackground, PlatformColorsAndroid.navigation),
+            },
+            title: {
+                color: platformNativeColor(undefined, PlatformColorsAndroid.onPrimaryText),
+            },
+            largeTitle: {
+                visible: false,
+            },
+            scrollEdgeAppearance: {
+                active: true,
+                noBorder: true,
+                background: {
+                    translucent: true,
+                    color: platformNativeColor(PlatformColorsIOS.secondarySystemBackground, PlatformColorsAndroid.navigation),
+                },
+            },
+            searchBar: {
+                visible: false,
+                hideOnScroll: true,
+                hideTopBarOnFocus: true,
+                obscuresBackgroundDuringPresentation: true,
+            },
+            hideNavBarOnFocusSearchBar: true,
+            searchBarHiddenWhenScrolling: true,
+            searchBarPlaceholder: localization.common.search,
+            noBorder: false,
+        },
+        bottomTabs: {
+            animate: true,
+            hideShadow: false,
+            translucent: true,
+            animateTabSelection: true,
+            preferLargeIcons: false,
+            tabsAttachMode: "together",
+            backgroundColor: platformNativeColor(PlatformColorsIOS.secondarySystemBackground, PlatformColorsAndroid.navigation),
+            ...Platform.select({
+                android: {
+                    translucent: false,
+                    borderWidth: 1,
+                    borderColor: platformNativeColor(undefined, PlatformColorsAndroid.divider),
+                },
+            }),
+        },
+        bottomTab: {
+            selectedTextColor: platformNativeColor(PlatformColorsIOS.systemBlue, PlatformColorsAndroid.onPrimaryText),
+            selectedIconColor: platformNativeColor(PlatformColorsIOS.systemBlue, PlatformColorsAndroid.onPrimaryText),
+            textColor: platformNativeColor(PlatformColorsIOS.secondaryLabel, PlatformColorsAndroid.onPrimaryTextOpacity),
+            iconColor: platformNativeColor(PlatformColorsIOS.secondaryLabel, PlatformColorsAndroid.onPrimaryTextOpacity),
+        },
+        statusBar: {
+            backgroundColor: platformNativeColor(undefined, PlatformColorsAndroid.statusbar),
+            visible: true,
+        },
+    });
 }
 
 export function registerComponents() {
-  if (__DEV__) {
-    Navigation.registerComponent(Pages.storybook.name, () => StorybookUIRoot);
-  }
-  Navigation.registerComponent(
-    Pages.splash.name,
-    () => gestureHandlerRootHOC(reduxProvider(Splash)),
-    () => Splash,
-  );
-  Navigation.registerComponent(Pages.onboarding.name, () => Onboarding);
-  Navigation.registerComponent(Pages.main.name, () => Main);
-  Navigation.registerComponent(Pages.search.name, () => Search);
-  Navigation.registerComponent(Pages.settings.name, () => Settings);
-  Navigation.registerComponent(Pages.toast.name, () => ToastOverlay);
-  Navigation.registerComponent(Pages.datePicker.name, () => DatePickerOverlay);
+    if (__DEV__) {
+        Navigation.registerComponent(Pages.storybook.name, () => StorybookUIRoot);
+    }
+    Navigation.registerComponent(
+        Pages.splash.name,
+        () => gestureHandlerRootHOC(reduxProvider(Splash)),
+        () => Splash,
+    );
+    Navigation.registerComponent(Pages.onboarding.name, () => Onboarding);
+    Navigation.registerComponent(Pages.main.name, () => Main);
+    Navigation.registerComponent(Pages.search.name, () => Search);
+    Navigation.registerComponent(Pages.settings.name, () => Settings);
+    Navigation.registerComponent(Pages.toast.name, () => ToastOverlay);
+    Navigation.registerComponent(Pages.datePicker.name, () => DatePickerOverlay);
 }
