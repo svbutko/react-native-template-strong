7e6fdc3171af0c1db13ff7e018839bf7f2563d0b
svbutko <svbutko@hotmail.com>
2023-06-05 23:51:13 +0300

    Add createAppAsyncThunk

--- template/src/core/store/store.tsx
+++ template/src/core/store/store.tsx
@@ -1,13 +1,8 @@
 import React from 'react';
-import {Action, configureStore, ThunkAction} from '@reduxjs/toolkit';
+import {Action, configureStore, createAsyncThunk, ThunkAction,} from '@reduxjs/toolkit';
 import {PersistConfig, persistReducer, persistStore} from 'redux-persist';
 import {rootReducer, RootState} from './rootReducer';
-import {
-  Provider,
-  TypedUseSelectorHook,
-  useDispatch,
-  useSelector,
-} from 'react-redux';
+import {Provider, TypedUseSelectorHook, useDispatch, useSelector,} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
@@ -21,26 +16,31 @@ const persistConfig: PersistConfig<RootState> = {
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: persistedReducer,
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: false,
-    }),
+    reducer: persistedReducer,
+    middleware: getDefaultMiddleware =>
+        getDefaultMiddleware({
+            serializableCheck: false,
+        }),
 });
 
 const persistor = persistStore(store);
 
-type AppDispatch = typeof store.dispatch;
+export type AppDispatch = typeof store.dispatch;
 export type AppThunk = ThunkAction<void, RootState, unknown, Action<string>>;
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
+export const createAppAsyncThunk = createAsyncThunk.withTypes<{
+    state: RootState;
+    dispatch: AppDispatch;
+}>();
+
 export const reduxProvider = (Component: any) => (props: any) => {
-  return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <Component {...props} />
-      </PersistGate>
-    </Provider>
-  );
+    return (
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                <Component {...props} />
+            </PersistGate>
+        </Provider>
+    );
 };
